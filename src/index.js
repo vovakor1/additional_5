0bb11f08431a3dca309ceea197f5310d8c7e25e4
Vladimir <v.korotkin11@gmail.com>
2018-09-29 22:32:02 +0300

    additional_5: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const config = bracketsConfig[0];
+  let brackets = str.split("");
+  for (let i = 0; i < config.length; i++) {
+      let a = 0;
+      let b = 0;
+      let c = config[i];
+      let d = config[i+1];
+
+      for (let j = 0; j < brackets.length; j++) {
+          if (brackets[j] === c) a++;
+          if (brackets[j] === d) b++;
+          if (b > a) return false;
+      }
+      return a === b;
+  }
 }
